805c5284c822cc344c6e07542aa4ef8d80c3f4c4
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-01-16 18:52:51 +0100

    day3: fixed bug and make the solution more elegant
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- 2021/daythree/solution.v
+++ 2021/daythree/solution.v
@@ -47,10 +47,10 @@ pub fn find_diagnostic_report(input_url &string) int {
 
 pub fn calculate_life_support(input_url &string) int {
 	status := prepare_input(input_url)
-	oxygen := oxygen_generator_rating(status, 0, 1)
-	co2 := co2_scrubber_rating(status, 0, 0)
-	println(oxygen)
-	println(co2)
+	oxygen := oxygen_generator_rating(status)
+	co2 := co2_scrubber_rating(status)
+	println('oxygen $oxygen')
+	println('co2 $co2')
 	return oxygen * co2
 }
 
@@ -60,32 +60,43 @@ pub fn calculate_life_support(input_url &string) int {
 // - build the most common bits table by index
 // - use the most common map result as input of the next recursive call
 // - is we have aquality in the most common bit at position index return the choice value
-fn oxygen_generator_rating(bits_list []string, index int, choice int) int {
-	cmp := fn (a int, b int) bool {
-		return a > b
+fn oxygen_generator_rating(bits_list []string) int {
+	cmp := fn (ones []string, zeros []string) []string {
+		if ones.len > zeros.len {
+			return ones
+		} else if ones.len < zeros.len {
+			return zeros
+		}
+		return ones
 	}
-	binary_number := life_support_helper(bits_list, index, choice, cmp)
+	binary_number := life_support_helper(bits_list, 0, cmp)
+	assert binary_number.len == 1
 	return to_decimal(binary_number[0])
 }
 
 // Calculate the co2 scribber rating
 //
 // Procedure s the same of oxygen_generator_rating
-fn co2_scrubber_rating(bits_list []string, index int, choice int) int {
-	cmp := fn (a int, b int) bool {
-		return a < b
+fn co2_scrubber_rating(bits_list []string) int {
+	cmp := fn (ones []string, zeros []string) []string {
+		if ones.len > zeros.len {
+			return zeros
+		} else if ones.len < zeros.len {
+			return ones
+		}
+		return zeros
 	}
-	binary_number := life_support_helper(bits_list, index, choice, cmp)
+	binary_number := life_support_helper(bits_list, 0, cmp)
+	assert binary_number.len == 1
 	return to_decimal(binary_number[0])
 }
 
 // Function that contains the recursion common logic to solve the life support
 // problem
-fn life_support_helper(bits_list []string, at int, def_choice int, cmp fn (int, int) bool) []string {
-	// TODO: build the most common bits map
-	// select the biggest bits, and return the winner list
-	// otherwise continue the recursion
-	// Commons list map
+fn life_support_helper(bits_list []string, at int, cmp fn ([]string, []string) []string) []string {
+	// 1. build the most common bits map
+	// 1.1 select the biggest bits, and return the winner list
+	// 1.2 otherwise continue the recursion
 	if bits_list.len == 1 {
 		println(bits_list)
 		return bits_list
@@ -93,7 +104,7 @@ fn life_support_helper(bits_list []string, at int, def_choice int, cmp fn (int, 
 	mut commons_bits := map[string][]string{}
 	for bits_item in bits_list {
 		assert bits_item.len > at
-		bit := bits_item[int(at)].ascii_str()
+		bit := bits_item[at].ascii_str()
 		if bit in commons_bits {
 			mut value := commons_bits[bit]
 			value << bits_item
@@ -107,17 +118,8 @@ fn life_support_helper(bits_list []string, at int, def_choice int, cmp fn (int, 
 	ones := commons_bits['1']
 	zeros := commons_bits['0']
 
-	if cmp(ones.len, zeros.len) {
-		return life_support_helper(ones, at + 1, def_choice, cmp)
-	} else if cmp(ones.len, zeros.len) {
-		return life_support_helper(zeros, at + 1, def_choice, cmp)
-	} else {
-		if def_choice == 1 {
-			return life_support_helper(ones, at + 1, def_choice, cmp)
-		} else {
-			return life_support_helper(zeros, at + 1, def_choice, cmp)
-		}
-	}
+	new_list := cmp(ones, zeros)
+	return life_support_helper(new_list, at + 1, cmp)
 }
 
 fn to_decimal(binary_str string) int {
