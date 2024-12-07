e7cc6104075ef83e99909cde9c1b07c8ffe6b7aa
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-12-07 13:54:18 +0100

    core: add solution template for day one
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ 2024/core/day1.go
@@ -0,0 +1,10 @@
+package core
+
+import "fmt"
+
+// SolveDay1 - Solve the first day of the Advent of Code 2024
+//
+// See the problem: https://adventofcode.com/2024/day/1
+func SolveDay1(rawInput string) (*string, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
--- 2024/main.go
+++ 2024/main.go
@@ -7,8 +7,13 @@ import (
 	"github.com/charmbracelet/log"
 
 	"github.com/vincenzopalazzo/AdventOfCode/2024/cmd"
+	"github.com/vincenzopalazzo/AdventOfCode/2024/core"
 	"github.com/vincenzopalazzo/AdventOfCode/2024/utils"
 )
+
+var Solutions = map[int]func(string) (*string, error){
+	1: core.SolveDay1,
+}
 
 func main() {
 	args := cmd.Parse()
@@ -21,15 +26,27 @@ func main() {
 		log.Infof("Running day `%d`", day)
 		// FIXME: load the file from input, and the file template is aoc2024_1.in
 		inFileName := fmt.Sprintf("aoc2024_day%d.in", day)
+		contentString := ""
 		if content, err := utils.ReadFile(inputPath, inFileName); err != nil {
 			log.Errorf("Error reading file %s", err)
 			os.Exit(1)
 		} else {
-			log.Infof("Content of file %s", *content)
+			contentString = *content
+		}
+
+		sol, ok := Solutions[day]
+		if !ok {
+			log.Errorf("Day %d not implemented", day)
+			os.Exit(1)
+		}
+		output, err := sol(contentString)
+		if err != nil {
+			log.Errorf("Error solving day %d: %s", day, err)
+			os.Exit(1)
 		}
 
 		outFileName := fmt.Sprintf("aoc2024_day%d.out", day)
-		if err := utils.WriteFile(outputPath, outFileName, "Hello World"); err != nil {
+		if err := utils.WriteFile(outputPath, outFileName, output); err != nil {
 			log.Errorf("Error writing file %s", err)
 			os.Exit(1)
 		}
